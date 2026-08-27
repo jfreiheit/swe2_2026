15c914085263eafb825f4ddcb0cc082e4985b1bd
Jörn Freiheit <freiheit@htw-berlin.de>
2026-08-27 10:40:48 +0200

    feat: GET /role/{id} – einzelne Rolle lesen
    Closes #10

--- studibackend/src/main/java/htw/freiheit/studi/controller/RoleController.java
+++ studibackend/src/main/java/htw/freiheit/studi/controller/RoleController.java
@@ -6,6 +6,7 @@ import htw.freiheit.studi.service.RoleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +33,10 @@ public class RoleController {
     @GetMapping
     public ResponseEntity<List<RoleResponseDTO>> findAll() {
         return ResponseEntity.ok(roleService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoleResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(roleService.findById(id));
     }
 }
