f116a736c85c8c218de1567ead568c66f37587ef
Jörn Freiheit <jfreiheit@users.noreply.github.com>
2026-08-27 10:39:47 +0200

    feat: GET /role – alle Rollen lesen (#23)
    Closes #9
    
    Co-authored-by: Jörn Freiheit <freiheit@htw-berlin.de>

--- studibackend/src/main/java/htw/freiheit/studi/controller/RoleController.java
+++ studibackend/src/main/java/htw/freiheit/studi/controller/RoleController.java
@@ -5,10 +5,13 @@ import htw.freiheit.studi.dto.RoleResponseDTO;
 import htw.freiheit.studi.service.RoleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/role")
@@ -24,5 +27,10 @@ public class RoleController {
     public ResponseEntity<RoleResponseDTO> create(@RequestBody RoleRequestDTO request) {
         RoleResponseDTO created = roleService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleResponseDTO>> findAll() {
+        return ResponseEntity.ok(roleService.findAll());
     }
 }
